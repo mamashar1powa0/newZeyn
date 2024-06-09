196d0e813f22934f03c62748063412ef9507c7be
mamashar1powa0 <zeyn0102030@gmail.com>
2024-06-09 10:50:24 +0600

    added files to githab

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import MainComoponentsReportCom from './components/MainComponents/MainComoponentsReportCom/MainComoponentsReportCom'
+import MainComponentsheader2 from './components/MainComponents/MainComponentsheader2/MainComponentsheader2'
+import MainComponentsidtovar from './components/MainComponents/MainComponentsidtovar/MainComponentsidtovar'
+
+function App() {
+	return (
+		<div>
+			<MainComoponentsReportCom />
+			<MainComponentsheader2 />
+			<MainComponentsidtovar/>
+		</div>
+	)
+}
+
+export default App
